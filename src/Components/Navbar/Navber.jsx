7b1628dd59dcac50e3a7a7620222a9a2ec3ee613
Kush  Mevada <102648785+Kush2343@users.noreply.github.com>
2024-03-26 14:41:23 +0530

    Update Navber.jsx

--- src/Components/Navbar/Navber.jsx
+++ src/Components/Navbar/Navber.jsx
@@ -6,6 +6,7 @@ import { HiDocumentText } from "react-icons/hi";
 import { FiCodesandbox } from "react-icons/fi";
 import { FaAddressBook } from "react-icons/fa";
 import { IoClose } from "react-icons/io5";
+import { NavLink } from 'react-router-dom';
 
 function Header({setisOpen}) {
   const [menuOpen, setMenuOpen] = useState();
@@ -32,7 +33,7 @@ function Header({setisOpen}) {
 <header className={`flex justify-between items-center sticky z-50 top-0 left-0 w-full lg:sticky lg:z-50 lg:px-20 ${isScrolling ? 'backdrop-blur-lg lg:py-3 transition-all duration-500 ease-in-out' : 'lg:py-9 transition-all duration-500 ease-in-out'}`}>
 <div className=" flex justify-between w-full px-4 bg-black lg:bg-transparent lg:px-0 py-3 lg:py-0">
         <div className="flex justify-between w-full items-center space-x-4">
-          <h1 className='text-4xl text-[--main-color]'>Darshil</h1>
+          <h1 className='text-4xl text-[--bg-color] font-extrabold'>Darshil Patel</h1>
           <div className="flex items-center">
             {!menuOpen ? (
               <span
@@ -67,59 +68,59 @@ function Header({setisOpen}) {
           }`}
         >
           <li>
-            <a
+            <NavLink
               className="bg-transparent lg:bg-black rounded-md pl-4 text-white font-medium mx-2.5 flex text-xtiny py-2.5 md:px-4 xl:px-5 items-center hover:text-[--main-color] hover:bg-[--bg-color] transition-all duration-300 ease-in-out"
-              href="/home"
+              to="/"
             >
               <span className="mr-3 text-xl">
               <MdHome className="text-[--main-color]" />
               </span>
               Home
-            </a>
+            </NavLink>
           </li>
           <li>
-            <a
+            <NavLink
               className="bg-transparent lg:bg-black rounded-md pl-4 text-white font-medium mx-2.5 flex text-xtiny py-2.5 md:px-4 xl:px-5 items-center  transition-all duration-300 ease-in-out hover:text-[--main-color] hover:bg-[--bg-color]"
-              href="/about"
+              to="/about"
             >
               <span className="mr-3 text-xl">
               <FaUser  className="text-[--main-color]" />
               </span>
               About
-            </a>
+            </NavLink>
           </li>
           <li>
-            <a
+            <NavLink
               className="bg-transparent lg:bg-black rounded-md pl-4 text-white font-medium mx-2.5 flex text-xtiny py-2.5 md:px-4 xl:px-5 items-center  transition-all duration-300 ease-in-out hover:text-[--main-color] hover:bg-[--bg-color]"
-              href="/resume"
+              to="/resume"
             >
               <span className="mr-3 text-xl">
               <HiDocumentText className="text-[--main-color]" />
               </span>
               Resume
-            </a>
+            </NavLink>
           </li>
           <li>
-            <a
+            <NavLink
               className="bg-transparent lg:bg-black rounded-md pl-4 text-white font-medium mx-2.5 flex text-xtiny py-2.5 md:px-4 xl:px-5 items-center  transition-all duration-300 ease-in-out hover:text-[--main-color] hover:bg-[--bg-color]"
-              href="/projects"
+              to="/projects"
             >
               <span className="mr-3 text-xl">
               <FiCodesandbox className="text-[--main-color]"/>
               </span>
               Projects
-            </a>
+            </NavLink>
           </li>
           <li>
-            <a
+            <NavLink
               className="bg-transparent lg:bg-black rounded-md pl-4 text-white font-medium mx-2.5 flex text-xtiny py-2.5 md:px-4 xl:px-5 items-center  transition-all duration-300 ease-in-out hover:text-[--main-color] hover:bg-[--bg-color]"
-              href="/contact"
+              to="/contact"
             >
               <span className="mr-3 text-xl">
               <FaAddressBook className="text-[--main-color]" />
               </span>
               Contact
-            </a>
+            </NavLink>
           </li>
         </ul>
       </nav>
